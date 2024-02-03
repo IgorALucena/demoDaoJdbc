6578e1d58b8158f567c9c5350c4a6ea06c28328e
Igor <igoralbuquerque021@gmail.com>
2024-02-03 20:37:18 -0300

    feat: implementação do update seller

--- src/apliccation/Program.java
+++ src/apliccation/Program.java
@@ -42,7 +42,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller update =====");
 		
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Martha");
+		sellerDao.update(seller);
+		
+		System.out.println("update completed");
 
 	}
 
